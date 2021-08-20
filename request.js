02b85bc186bbe765634e82cf2e6a1d19feb43c55
wnstjd9701 <wnstjd9701@naver.com>
2021-08-20 10:47:46 +0900

    CreateServer without Express

---
+++ request.js
@@ -0,0 +1,16 @@
+const http = require('http'); // Nodejs MOdule
+
+http.createServer( (request, response) => {  
+    response.writeHead(200, {'Content-Type' : 'text/plain'});
+    response.write('Hello Server');
+    response.end();
+}).listen(80); // xx번 포트 사용
+
+
+/*
+1xx = 조건부응답
+2xx = 응답성공
+3xx = 리다이렉션
+4xx = 요청오류
+5xx = 서버오류
+*/
