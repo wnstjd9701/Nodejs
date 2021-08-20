7593a536cd4b582e2aca731ba8f46f0704e78eed
wnstjd9701 <wnstjd9701@naver.com>
2021-08-20 11:01:11 +0900

    Express

---
+++ Express/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+// url 을 추가하려면 routing 필요!!
+app.get('/', (req, res) =>{
+    res.send('Hello express');
+})
+
+app.get('/test', (req, res)=>{
+    res.send('Hello express - test url...'); // response
+})
+
+app.listen( port, ()=>{
+    console.log('Express listening on port', port);
+})
