f04137859292e9e3fe525661bd3464bfb296c7d3
wnstjd9701 <wnstjd9701@naver.com>
2021-08-29 23:29:45 +0900

    2021-08-29

--- Express/server.js
+++ Express/server.js
@@ -1,6 +1,8 @@
 const app = require('./app');
 const port = 3000;
+const test = 'hello world';
 
 app.listen( port, ()=>{
     console.log('Express listening on port', port);
+    console.log(test);
 });
