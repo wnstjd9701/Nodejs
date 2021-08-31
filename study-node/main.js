073c28c628bf0fd7984f0aa5dd9c374ecfbf6f0a
wnstjd9701 <wnstjd9701@naver.com>
2021-08-31 17:47:41 +0900

    every

--- study-node/main.js
+++ study-node/main.js
@@ -1,18 +1,7 @@
-// 큐 . FIFO - First in first out 
+'use strict'
 
-const queue = [];
-queue.push(1);
-queue.push(2);
-console.log(queue);
+const arr = [2,3,4]
 
-const r = queue.shift(); // shift - 첫번째 요소를 제거하고 반환
-console.log(r);
+const isBiggerThanOnde = arr.every(key => key > 2 )
 
-// 스택 . LIFO - Last in first out 
-const stack = [];
-stack.push(1)
-stack.push(2)
-console.log(stack);
-
-var s = stack.pop() // pop - 마지막 요소를 제거하고 반환
-console.log(s)
+console.log(isBiggerThanOnde)
