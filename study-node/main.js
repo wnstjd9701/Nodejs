be7625822254536f876e187af8076d74b705d986
wnstjd9701 <wnstjd9701@naver.com>
2021-08-31 17:53:18 +0900

    Find,includes

--- study-node/main.js
+++ study-node/main.js
@@ -1,7 +1,16 @@
 'use strict'
 
-const arr = [2,3,4]
+const { resolve } = require("path")
 
-const isBiggerThanOnde = arr.every(key => key > 2 )
+const arr = ['node.js', '올인원']
+const test = ['hello world']
+const ret = arr.find(key => key === '올인원') // find - 실제 데이터 return 
+const res = arr.includes('node.js') // includes - 포함하고 있는가 
 
-console.log(isBiggerThanOnde)
+for(const item of test){
+    if(arr.includes(item)){
+        console.log(item)
+    }else{
+        console.log('Nothing')
+    }
+}
