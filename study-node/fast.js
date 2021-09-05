df215e0d4cdf901e94be169884a0a950cbbb3236
wnstjd9701 <wnstjd9701@naver.com>
2021-09-05 20:20:15 +0900

    Typeof

--- study-node/fast.js
+++ study-node/fast.js
@@ -1,18 +1,7 @@
 'use strict'
 
-let string = 'node.js 올인원 패키지'
-
-let isStartWith = string.startsWith('n') // '' 단어로 시작 하는지 
-let isIncludes = string.includes('올인원') // '' 포함하고 있는지
-let isEndWith = string.endsWith('지') // '' 로 끝나는지 
-
-console.log(isStartWith)
-
-const checkIfContains = () => {
-    if(isStartWith && isIncludes && isEndWith){
-        return true
-    }
-}
-
-const ret = checkIfContains()
-console.log(ret)
+const string = `node.js` // type : string
+const arr = [] // type : object
+const obj = {} // type : object
+const num = 1 // type : number
+console.log(typeof arr)
