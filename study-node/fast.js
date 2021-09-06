1e36388b31d5d130f1277a1cf9f36495f2059772
wnstjd9701 <wnstjd9701@naver.com>
2021-09-06 17:49:41 +0900

    2021-09-06

--- study-node/fast.js
+++ study-node/fast.js
@@ -1,5 +1,10 @@
-try{
-    a
-} catch (e) {
-    console.log('Err ' + e)
+'use strict'
+// Arrow Function
+
+const add = function add(var1, var2){
+    return var1 + var2 
 }
+
+const val = (var1, var2) => var1 + var2
+
+console.log(val(1,2))
