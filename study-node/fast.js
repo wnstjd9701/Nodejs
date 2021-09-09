d26da0a6a2d4dcc0c87c610bdd7bef0e980ce85b
wnstjd9701 <wnstjd9701@naver.com>
2021-09-09 18:24:07 +0900

    arrow function

--- study-node/fast.js
+++ study-node/fast.js
@@ -1,10 +1,19 @@
-'use strict'
-// Arrow Function
+// Curried Function .. 합성함수
+// f(x) = x + 1
+// g(x) = x^2
+// h(x) = f(g(x)) - g(X)가 먼저 호출  - deep learning 
 
-const add = function add(var1, var2){
-    return var1 + var2 
-}
-
-const val = (var1, var2) => var1 + var2
-
-console.log(val(1,2))
+const getDiscount = rate => price => rate * price
+// rate를 파라미터로 받음. 뒤에 price를 또 받고  (closure) price에 대해 함수를 선언
+// const getDiscount = function getDiscount(rate){
+//     return function(price){
+//         return rate * price
+//     }
+// }
+const getTenpercentOff = getDiscount(0.1)
+// getDiscount(rate)
+console.log(getTenpercentOff(10000))
+console.log(getTenpercentOff(10000))
+console.log(getTenpercentOff(10000))
+console.log(getTenpercentOff(10000))
+console.log(getTenpercentOff(10000))
