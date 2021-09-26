6148a6345c1e21b7c42166b598949779f1b7308c
wnstjd9701 <wnstjd9701@naver.com>
2021-09-26 20:12:59 +0900

    Querystring/Params

--- API/test.js
+++ API/test.js
@@ -8,7 +8,7 @@ app.get('/:main1/:main2', async (req, res) => {
     } = req.params
     console.log(test1);
     console.log(test2);
-    console.log(req.query.id);
+    console.log(req.query.id); // http://localhost:3000/test1/test3?id=querystring => 출력 req.query.id = querystring
     console.log(req.params.test1);
     // return res.render();
 });
