669b1d3e54664b35c836176a338727711323bb62
wnstjd9701 <wnstjd9701@naver.com>
2021-09-26 20:46:28 +0900

    Query/Params

--- API/test.js
+++ API/test.js
@@ -1,18 +1,33 @@
 const express = require('express');
 const app = express();
 
-app.get('/:main1/:main2', async (req, res) => {
+app.get('/:test1/:test2', async (req, res) => {
     let {
         test1,
         test2
     } = req.params
     console.log(test1);
     console.log(test2);
-    console.log(req.query.id); // http://localhost:3000/test1/test3?id=querystring => 출력 req.query.id = querystring
-    console.log(req.params.test1);
-    // return res.render();
+    console.log(req.query.test); // http://localhost:3000/test1/test3?id=querystring => 출력 req.query.id = querystring
+    console.log(req.params);
+    // // return res.render();
 });
 
+app.get('/topic', (req,res) => {
+    var topics = [
+        'Javascript is...',
+        'Nodejs is...',
+        'Express is...'
+    ];
+    var output = `
+        <a href ='/topic?id=0'> Javascript!! </a><br>
+        <a href ='/topic?id=1'> Nodejs!! </a><br>
+        <a href ='/topic?id=2'> Express!! </a>
+        <br>
+        ${topics[req.query.id]}
+        `
+    res.send(output);
+});
 
 
 app.listen(3000, () => {
