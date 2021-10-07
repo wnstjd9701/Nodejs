5351c907d771964b75a14fe5df3d3f49756e6ea4
Yun Jun Sung <wnstjd9701@naver.com>
2021-10-07 18:02:51 +0900

    GithubAPI

--- REST_API/test.js
+++ REST_API/test.js
@@ -1,5 +1,7 @@
 const test1 = { id: "a", name: "LEE", email: "LEE@naver.com", test1: "test1"};
 const test2 = { id: "b", name: "Yun", email: "Yun@naver.com", test2: "test2"};
 
+// copy
 const merge = {...test1, ...test2};
 console.log(merge);
+
