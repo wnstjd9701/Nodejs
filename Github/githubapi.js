bc884464c672cbac65d61dde818619bc758b59ae
Yun Jun Sung <wnstjd9701@naver.com>
2021-10-07 18:25:42 +0900

    GithubAPI

--- Github/githubapi.js
+++ Github/githubapi.js
@@ -2,29 +2,16 @@ const { appendFile } = require('fs');
 var GitHub = require('github-api');
 var noAuth = new GitHub();
 
-// // test
-// exports.test = async function(req, res){
-//     console.log(req.params);
-// }
-
-
 // 사용자 레포지토리 리스트 조회
-exports.getUserRepositoryList = async function(req, res){
+exports.getUserRepositoryList = function(req, res){
     var repositoryList = noAuth.getUser(`${req.params.userId}`);
-    var repoInfo = {
-        repoOwner: "",
-        repository: {
-            name: []
-        }
-    };
 
     repositoryList.listRepos(function(err,repos){
         repos.forEach((repo) => {
             console.log(`"${repo.name} " -- "${repo.owner.login}"`)
             repoInfo.repository.name.push(repo.name);
-            //console.log(repoInfo.repository.name);
         })
         // repoInfo.repoOwner.push(repos.owner.login);
+        res.json(repos);
     })
-    console.log(repoInfo);
 }
