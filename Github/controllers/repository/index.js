2acf3b172ca25826737a713027068a516d1bce23
Yun Jun Sung <wnstjd9701@naver.com>
2021-10-08 14:13:26 +0900

    GithubAPI

--- Github/controllers/repository/index.js
+++ Github/controllers/repository/index.js
@@ -1,7 +1,9 @@
 const { Router } = require('express');
 const router = Router();
 const githubapi = require('./githubapi');
+const test = require('./test');
 
 router.get('/userRepository/:userId', githubapi.getUserRepositoryList);
+// router.get('/userRepoList', test.ghrepos);
 
 module.exports = router;
---
+++ Github/controllers/repository/test.js
@@ -0,0 +1,8 @@
+// const ghrepos = require('ghrepos');
+// const authOptions = { use: "wnstjd9701", token: process.end.PAT }
+
+// exports.ghrepos.listUser(authOptions, "wnstjd9701", (err, repolist) => {
+//     repolist.forEach(repo => {
+//         console.log(repo);
+//     });
+// })
