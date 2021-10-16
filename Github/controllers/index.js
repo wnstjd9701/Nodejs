4bc9393cd8aa11852b655970f37655a0411caf5a
Yun Jun Sung <wnstjd9701@naver.com>
2021-10-16 21:04:09 +0900

    2021-10-16

--- Github/controllers/index.js
+++ Github/controllers/index.js
@@ -3,5 +3,7 @@ const router = Router();
 
 router.use('/', require('./Repository')); // 폴더 지정 .. URL/repository/userRepositoryList/:userId
 router.use('/', require('./MyPage'));
-
+router.use('/test', () => {
+  res.json({ message: 'test' });
+});
 module.exports = router;
