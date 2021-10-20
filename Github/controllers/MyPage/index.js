4677b44140d0f1e687f1ba5ed05de3e9d4857452
Yun Jun Sung <wnstjd9701@naver.com>
2021-10-20 14:20:20 +0900

    Github

--- Github/controllers/MyPage/index.js
+++ Github/controllers/MyPage/index.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const router = Router();
 const myprofile = require('./myprofile');
 
+router.get('/', myprofile.getUserProfile);
 router.get('/:userId', myprofile.getUserProfile);
 
 module.exports = router;
-
