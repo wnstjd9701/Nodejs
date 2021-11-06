9f45ec3aaea4bdb58cf637336f6cb53163ab6bed
Yun Jun Sung <wnstjd9701@naver.com>
2021-11-06 18:51:06 +0900

    Object Array

--- API/test.js
+++ API/test.js
@@ -2,34 +2,22 @@ const express = require('express');
 const app = express();
 // exoress
 app.get('/:test1/:test2', async (req, res) => {
-    let {
-        test1,
-        test2
-    } = req.params
-    console.log(test1);
-    console.log(test2);
-    console.log(req.query.test); // http://localhost:3000/test1/test3?id=querystring => 출력 req.query.id = querystring
-    console.log(req.params);
-    // // return res.render();
+  let { test1, test2 } = req.params;
+  console.log(test1);
+  console.log(test2);
+  console.log(req.query.test); // http://localhost:3000/test1/test3?id=querystring => 출력 req.query.id = querystring
+  console.log(req.params);
+  // // return res.render();
 });
 
 // frontend id -> backend server req.
-app.get('/topic', (req,res) => {
-    var topics = [
-        'Javascript is...',
-        'Nodejs is...',
-        'Express is...'
-    ];
-    var output = `
-        <a href ='/topic?id=0'> Javascript!! </a><br>
-        <a href ='/topic?id=1'> Nodejs!! </a><br>
-        <a href ='/topic?id=2'> Express!! </a><br>
-        ${topics[req.query.id]}
-        `
-    res.send(output);
+app.get('/topic', (req, res) => {
+  const obj = {
+    advancedTeckStack: ['c++', 'c', 'java', 'javascript'],
+  };
+  console.log(obj.advancedTeckStack[0]);
 });
 
-
 app.listen(3000, () => {
-    console.log('status ok');
+  console.log('status ok');
 });
