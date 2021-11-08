361e0bbdcf9a5aa717030810ddb3bd24d9a3b1a2
Yun Jun Sung <wnstjd9701@naver.com>
2021-11-08 21:04:46 +0900

    2021-11-08

--- API/test.js
+++ API/test.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 // exoress
 app.get('/:test1/:test2', async (req, res) => {
   let { test1, test2 } = req.params;
@@ -11,12 +13,32 @@ app.get('/:test1/:test2', async (req, res) => {
 });
 
 // frontend id -> backend server req.
-app.post('/topic', (req, res) => {
+app.post('/topic/:userId', (req, res) => {
   // const techStack = {
   //   advancedTechStack: ['c++', 'c', 'java', 'javascript'],
   //   experiencedTechStack: ['python'],
   // };
-  console.log(req.body);
+  const techStack = req.body;
+  const { advanced, experienced } = req.body;
+  console.log(advanced);
+  //console.log(techStack);
+
+  try {
+    if (experienced[0] == undefined || experienced[0].name == '') {
+      console.log(experienced[0].name);
+      console.log('test');
+    } else {
+      console.log(experienced[0].name);
+    }
+    console.log(techStack.advancedTechStack);
+    advanced.forEach(function (item, index) {
+      const techName = item.name;
+      console.log(techName, index);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+  res.json({ ErrorMessage: 'error' });
 });
 
 app.listen(3000, () => {
