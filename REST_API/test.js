bf140100a8b68e30f68fef9a73d945215726ca58
Yun Jun Sung <wnstjd9701@naver.com>
2021-11-17 18:42:50 +0900

    2021-11-17

--- REST_API/test.js
+++ REST_API/test.js
@@ -6,7 +6,7 @@ const { default: axios } = require('axios');
 const { appendFile } = require('fs');
 const GitHub = require('github-api'); // npm install github-api
 const noAuth = new GitHub(); // noAuth 방식 -> token, noAuth,
-
+// api test
 const api = axios.create({
   baseURL: 'https://api.github.com',
 });
