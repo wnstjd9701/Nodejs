b9e210691788c742dae08934b4ad1548ad476647
Yun Jun Sung <wnstjd9701@naver.com>
2021-12-07 01:03:58 +0900

    2021-12-07

--- Express/server.js
+++ Express/server.js
@@ -1,7 +1,7 @@
 const app = require('./app.js');
 const port = 3000;
 
-app.listen( port, () => {
-    console.log('Express listening on port', port);
+app.listen(port, () => {
+  console.log('Express listening on port', port);
 });
-// port 3000 , listen server
+// port 3000 , listen server.
